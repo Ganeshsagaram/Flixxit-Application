1d8582ab1cdefc730ad5903d326e176928cc16ba
ganesh sagaram <ganeshsai1504@gmail.com>
2025-06-19 18:05:05 +0530

    Changed the Mongo URI now it will connect to Atlas Service

--- backend-service/DB-Connections/mongoConnect.js
+++ backend-service/DB-Connections/mongoConnect.js
@@ -3,7 +3,7 @@ import { MongoClient } from "mongodb";
 import dotenv from 'dotenv';
 dotenv.config();
 export async function dbConnection() {
-    const uri="mongodb+srv://ganesh:Venkatsai%401504@myatlasclusteredu.liqdar6.mongodb.net/?retryWrites=true&w=majority&appName=myAtlasClusterEDU"
+    const uri=process.env.MONGO_URI;
     const client=new MongoClient(uri);
       const connection=await client.connect();
       return connection;
